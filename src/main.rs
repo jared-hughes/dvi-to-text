2b5c274782559a996096fbcf2c5644c6d18a5d0c
Jared Hughes <jahughes241@gmail.com>
2022-10-28 02:14:01 -0700

    Iterate through DVI file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
-use std::fs;
+// Treat the entire input as a single batch
+// TODO: stream the input, stdin or something
+fn text(dvi_bytes: &[u8]) {
+    let mut bytes_remaining = dvi_bytes;
+    while bytes_remaining.len() > 0 {
+        match dvi::Instruction::parse(bytes_remaining) {
+            Err(e) => eprintln!("Error parsing DVI file: {e:?}"),
+            Ok((b, inst)) => {
+                use dvi::Instruction::*;
+                match inst {
+                    Set(charcode) | Put(charcode) => println!("Typeset char {charcode:x}"),
+                    Bop(_c, _p) => println!("Beginning of page"),
+                    Eop => println!("Ending of page"),
+                    Down(y) => println!("Down by {y}"),
+                    Y(y) => println!("Down by {y:?} and set y spacing"),
+                    // ignore horizontal movement, font changes, ...
+                    _ => {}
+                }
+                bytes_remaining = b;
+            }
+        }
+    }
+}
 
+/// temporary usage: cargo run ../tex-output/long.dvi
 fn main() {
-    match fs::read("../tex-output/long.dvi") {
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+    match std::fs::read(path) {
         Err(e) => eprintln!("Error reading DVI file: {e:?}"),
         Ok(bytes) => {
-                    println!("Loaded a dvi file with {} bytes", bytes.len());
-            match dvi::Instruction::parse(bytes.as_slice()) {
-                Err(e) => eprintln!("Error parsing DVI file: {e:?}"),
-                Ok(x) => {
-                    println!("After first parse, {} bytes", x.0.len());
-                }
-            }
+            println!("Loaded a dvi file with {} bytes", bytes.len());
+            text(bytes.as_slice());
         }
     }
 }
