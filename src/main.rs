87e51d8b00b16742a1aeacd37830fb8b8e3998be
Jared Hughes <jahughes241@gmail.com>
2022-10-28 00:21:53 -0700

    Add dvi crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    match fs::read("../tex-output/long.dvi") {
+        Err(e) => eprintln!("Error reading DVI file: {e:?}"),
+        Ok(bytes) => {
+                    println!("Loaded a dvi file with {} bytes", bytes.len());
+            match dvi::Instruction::parse(bytes.as_slice()) {
+                Err(e) => eprintln!("Error parsing DVI file: {e:?}"),
+                Ok(x) => {
+                    println!("After first parse, {} bytes", x.0.len());
+                }
+            }
+        }
+    }
 }
