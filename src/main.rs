44c44f6f3b37e131c724edaecd82354539066c57
Jared Hughes <jahughes241@gmail.com>
2022-11-03 15:27:46 -0700

    Add message for incorrect argument count

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,11 @@ fn text(dvi_bytes: &[u8]) {
 /// temporary usage: cargo run ../tex-output/long.dvi
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Incorrect number of arguments.");
+        println!("Usage: dvi-to-text filename.dvi");
+        return;
+    }
     let path = &args[1];
     match std::fs::read(path) {
         Err(e) => eprintln!("Error reading DVI file: {e:?}"),
